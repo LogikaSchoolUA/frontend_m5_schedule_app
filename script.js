fa0ea5b501a62258aa680b66edf77531f1083083
LogikaSchoolUA <115147516+LogikaSchoolUA@users.noreply.github.com>
2024-06-18 02:45:30 +0300

    new: adding classes and save to cookies

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
+// Знаходимо всі елементи днів
 let days = document.querySelectorAll('#days li');
-let classes = {
-    "Понеділок": ['Алгебра', 'Англійська', 'Історія', 'Фізкультура'],
-    "Вівторок": ['Фізика', 'Хімія', 'Українська мова', "Інформатика"],
-    "Середа": ['Географія', 'Мистецтво', 'Українська література'],
-    "Четвер": ['Геометрія', 'Українська мова', 'Інформатика'],
-    "П'ятниця": [ 'Англійська', 'Алгебра', 'Фізкультура']
+
+// Об'єкт для зберігання розкладу та домашніх завдань
+let schedule = {
+    "Понеділок": { classes: [], homework: {} },
+    "Вівторок": { classes: [], homework: {} },
+    "Середа": { classes: [], homework: {} },
+    "Четвер": { classes: [], homework: {} },
+    "П'ятниця": { classes: [], homework: {} }
 };
 
-// Об'єкт для зберігання домашніх завдань
-let homework = {
-    "Понеділок": {},
-    "Вівторок": {},
-    "Середа": {},
-    "Четвер": {},
-    "П'ятниця": {}
-};
+// Розділяємо всі куки на окремі частини
+let cookies = document.cookie.split('; ');
+for (let i = 0; i < cookies.length; i += 1) {
+    let cookie = cookies[i].split('=');
+    if (cookie[0] === 'schedule') {
+        schedule = JSON.parse(cookie[1]);
+        break;
+    }
+}
 
-
+// Функція для збереження даних у кукі-файл
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
 // Додаємо обробник подій для кожного дня
 for (let i = 0; i < days.length; i++) {
@@ -25,17 +38,8 @@ for (let i = 0; i < days.length; i++) {
         highlightActiveDay(days[i]); // Підсвічуємо обраний день
         updateClassList(selectedDay); // Оновлюємо список занять
         updateHomeworkList(selectedDay); // Оновлюємо список домашніх завдань
-        // Встановлюємо кнопку для додавання домашнього завдання
-        let addHomeworkButton = document.querySelector('#add-homework');
-        addHomeworkButton.onclick = function() {
-            let selectedSubject = document.querySelector('#subject-select').value;
-            let newHomework = document.querySelector('#new-homework').value;
-            if (selectedSubject && newHomework) {
-                homework[selectedDay][selectedSubject] = newHomework; // Додаємо або перезаписуємо домашнє завдання
-                updateHomeworkList(selectedDay); // Оновлюємо список домашніх завдань
-                document.querySelector('#new-homework').value = ''; // Очищуємо поле вводу
-            }
-        };
+        setupAddHomeworkButton(selectedDay); // Встановлюємо кнопку для додавання домашнього завдання
+        setupAddClassButton(selectedDay); // Встановлюємо кнопку для додавання нового уроку
     });
 }
 
@@ -47,18 +51,16 @@ function highlightActiveDay(activeDay) {
     activeDay.classList.add('active');
 }
 
-
-
 // Функція для оновлення списку занять
 function updateClassList(day) {
-    document.querySelector('#day-name').innerHTML = day;// Оновлюємо назву обраного дня
+    document.querySelector('#day-name').innerHTML = day; // Оновлюємо назву обраного дня
     let classList = document.querySelector('#classes');
     classList.innerHTML = '';
     let subjectSelect = document.querySelector('#subject-select');
     subjectSelect.innerHTML = '<option value="" disabled selected>Виберіть предмет</option>';
-    for (let k = 0; k < classes[day].length; k++) {
-        let classItem = classes[day][k];
-        classList.innerHTML += `<li>${k+1}. ${classItem}</li>`;
+    for (let k = 0; k < schedule[day].classes.length; k++) {
+        let classItem = schedule[day].classes[k];
+        classList.innerHTML += `<li>${k + 1}. ${classItem}</li>`;
         let option = `<option value="${classItem}">${classItem}</option>`;
         subjectSelect.innerHTML += option;
     }
@@ -68,10 +70,43 @@ function updateClassList(day) {
 function updateHomeworkList(day) {
     let homeworkList = document.querySelector('#homework-list');
     homeworkList.innerHTML = '';
-    for (let subject in homework[day]) {
-        if (homework[day].hasOwnProperty(subject)) {
-            let hw = homework[day][subject];
-            homeworkList.innerHTML += `<li><input type="checkbox">${subject}:  ${hw}</li>`;
+    for (let subject in schedule[day].homework) {
+        if (schedule[day].homework.hasOwnProperty(subject)) {
+            let hw = schedule[day].homework[subject];
+            let completed = hw.completed ? 'checked' : '';
+            homeworkList.innerHTML += `<li><input type="checkbox"> ${subject}: ${hw.description}</li>`;
         }
     }
+}
+
+// Функція для налаштування кнопки додавання домашнього завдання
+function setupAddHomeworkButton(day) {
+    let addHomeworkButton = document.querySelector('#add-homework');
+    addHomeworkButton.onclick = function() {
+        let selectedSubject = document.querySelector('#subject-select').value;
+        let newHomework = document.querySelector('#new-homework').value;
+        if (selectedSubject && newHomework) {
+            schedule[day].homework[selectedSubject] = {
+                description: newHomework,
+                completed: false
+            };
+            updateHomeworkList(day);
+            setCookie("schedule", JSON.stringify(schedule), 14);
+            document.querySelector('#new-homework').value = '';
+        }
+    };
+}
+
+// Функція для налаштування кнопки додавання нового уроку
+function setupAddClassButton(day) {
+    let addClassButton = document.querySelector('#add-class-button');
+    addClassButton.onclick = function() {
+        let newClass = document.querySelector('#new-class').value;
+        if (newClass) {
+            schedule[day].classes.push(newClass);
+            updateClassList(day);
+            document.querySelector('#new-class').value = '';
+            setCookie("schedule", JSON.stringify(schedule), 14);
+        }
+    };
 }
